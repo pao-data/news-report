d2b6a8b13283f79141435fe9508015e6cddf6fec
Lindsay Eddy <eddy_lindsay@bah.com>
2026-06-01 22:41:54 +0000

    Convert to multi-page app.

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,30 +1,4 @@
-import logging
-from datetime import datetime
-
 import streamlit as st
 
-from utils.paths import BASE_DIR
-from ui.search import show_search_section
-from ui.report import show_report_section
-
-
-logging.basicConfig(
-        filename=BASE_DIR / f"logs/main_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log",
-        level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-if __name__ == "__main__":
-    if "articles" not in st.session_state:
-        st.session_state["articles"] = []
-    if "user_provided_template" not in st.session_state:
-        st.session_state["user_provided_template"] = None
-    if "show_search_results" not in st.session_state:
-        st.session_state["show_search_results"] = False
-
-    st.title("Morning Report")
-
-    show_search_section()
-    show_report_section()
-    
+pg = st.navigation([st.Page("pages/home.py"), st.Page("pages/convert_html.py")])
+pg.run()
