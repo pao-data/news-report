7ad1bd6b7ae7b981f3bab02c00ba807668d544d4
Lindsay Eddy <eddy_lindsay@bah.com>
2026-06-03 16:21:32 +0000

    Prototype page to extract text from raw html

--- app/pages/convert_html.py
+++ app/pages/convert_html.py
@@ -1,3 +1,25 @@
+from xml.dom import minidom
+
 import streamlit as st
 
+import core.extraction
+
 st.write("Extract article text from html")
+
+user_input = st.text_area(
+    label="Insert raw HTML text",
+    placeholder="Paste raw HTML text here",
+    height="content"
+)
+
+extracted_text = core.extraction.extract_main_text(raw_html=user_input) #, output_format="xml")
+# extracted_text = json.loads(extracted_text)
+
+if user_input:
+    st.divider()
+    st.text(extracted_text)
+    # pretty_xml = minidom.parseString(extracted_text).toprettyxml(indent="  ")
+    # st.text(pretty_xml)
+    # for k, v in extracted_text.items():
+    #     st.write(f"**{k}**")
+    #     st.write(v)
