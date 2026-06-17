ef35822eafb0d3a3779824497dd0e9068c9a9f2f
Lindsay Eddy <eddy_lindsay@bah.com>
2026-06-17 21:58:47 +0000

    Improve user query parsing.

--- app/core/search.py
+++ app/core/search.py
@@ -1,14 +1,26 @@
 import feedparser
 import logging
+import re
+from urllib.parse import quote
 
 from models.article import Article
 
 logger = logging.getLogger(__name__)
 
+def create_search_url(user_query: str):
+    normalized = re.sub(r"\s+", " ", user_query).strip()
+
+    google_query = f"{normalized} when:1d"
+
+    rss_url = (
+        "https://news.google.com/rss/search?"
+        f"q={quote(google_query)}"
+    )
+    return rss_url
 
 def get_articles_from_rss(query: str, limit: int | None = None) -> Article:
     """Get articles from Google RSS for the given search query within the past 24 hours."""
-    rss_url = "https://news.google.com/rss/search?q=" + query.replace(" ", "%20").replace('"', '%22') + "%20when:1d"
+    rss_url = create_search_url(query)
     feed = feedparser.parse(rss_url)
 
     logger.info(f"Feed status: {feed.get('status')}")
