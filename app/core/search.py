39fe5151b479fcf188eca4a15b1beaf3c0c59912
Lindsay Eddy <eddy_lindsay@bah.com>
2026-06-17 21:25:42 +0000

    Replace quotes in query with %22.

--- app/core/search.py
+++ app/core/search.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 def get_articles_from_rss(query: str, limit: int | None = None) -> Article:
     """Get articles from Google RSS for the given search query within the past 24 hours."""
-    rss_url = "https://news.google.com/rss/search?q=" + query.replace(" ", "%20") + "%20when:1d"
+    rss_url = "https://news.google.com/rss/search?q=" + query.replace(" ", "%20").replace('"', '%22') + "%20when:1d"
     feed = feedparser.parse(rss_url)
 
     logger.info(f"Feed status: {feed.get('status')}")
