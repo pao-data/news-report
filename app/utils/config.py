0cf462975c6e3c01c8e57243e986d02a26e808ff
Lindsay Eddy <eddy_lindsay@bah.com>
2026-06-18 16:01:34 +0000

    Add default search booleans

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import tomllib
 
+import yaml
+
 from utils.paths import BASE_DIR
 
 def load_config():
     config_path = BASE_DIR / "config.toml"
     with open(config_path, "rb") as f:
         return tomllib.load(f)
+    
+def load_default_queries():
+    yaml_path = BASE_DIR / "app/assets/default_search_booleans.yaml"
+    with open(yaml_path, "r") as f:
+        return yaml.safe_load(f)
