3759ed72a97182a0120d830e2ced360d0db0e3f9
Lindsay Eddy <eddy_lindsay@bah.com>
2026-07-14 17:37:45 -0400

    Linting/formatting changes.

--- app/ui/layout.py
+++ app/ui/layout.py
@@ -1,16 +1,17 @@
 import streamlit as st
-from streamlit_sortables import sort_items
-
-from models.section import Section
-from models.layout import Layout
-from utils.config import load_default_section_names
 import ui.state
+from models.layout import Layout
+from models.section import Section
+from streamlit_sortables import sort_items
+from utils.config import load_default_section_names
+
 
 def initialize_layout():
     section_names = load_default_section_names()
     ui.state.set_layout(Layout(section_names=section_names))
     st.rerun()
-    
+
+
 def show_layout_section():
     st.header("Organize Report")
     st.subheader("Manage Report Sections")
@@ -18,18 +19,17 @@ def show_layout_section():
     st.subheader("Manage Articles")
     show_section_tabs()
 
+
 def show_manage_sections():
     layout = ui.state.get_layout()
     # Show each section in order. When a user moves a section, update the Layout and the UI.
-    sections = get_numbered_list(
-        [s.name for s in layout.get_ordered_sections()]
-    )
+    sections = get_numbered_list([s.name for s in layout.get_ordered_sections()])
     reordered_sections = sort_items(sections, direction="vertical")
     if sections != reordered_sections:
         old_position, new_position = find_move(sections, reordered_sections)
         layout.reorder_section(old_position, new_position)
         st.rerun()
-    
+
     col, _ = st.columns([4, 10])
     col.text_input(
         "",
@@ -37,14 +37,14 @@ def show_manage_sections():
         placeholder="➕ Add new section (Enter section name and press Enter)",
         label_visibility="collapsed",
         on_change=add_section_from_text_input,
-        kwargs={"widget_key": "new_section_text_input"}
+        kwargs={"widget_key": "new_section_text_input"},
     )
+
 
 def add_section_from_text_input(widget_key):
     layout = ui.state.get_layout()
-    layout.add_section(
-        section_name=st.session_state[widget_key]
-    )
+    layout.add_section(section_name=st.session_state[widget_key])
+
 
 def show_section_tabs():
     layout = ui.state.get_layout()
@@ -65,15 +65,23 @@ def show_articles(section: Section):
         c1, c2, c3, c4 = st.columns([0.03, 0.03, 0.92, 0.03])
         if i > 0:
             c1.button(
-                "⬆", key=f"up_{article_id}", use_container_width=True, type="tertiary",
+                "⬆",
+                key=f"up_{article_id}",
+                use_container_width=True,
+                type="tertiary",
                 help="Move up within section.",
-                on_click=section.move_article_up, kwargs={"article_position": i}
+                on_click=section.move_article_up,
+                kwargs={"article_position": i},
             )
-        if i < len(section.articles)-1:
+        if i < len(section.articles) - 1:
             c2.button(
-                "⬇", key=f"down_{article_id}", use_container_width=True, type="tertiary",
+                "⬇",
+                key=f"down_{article_id}",
+                use_container_width=True,
+                type="tertiary",
                 help="Move down within section.",
-                on_click=section.move_article_down, kwargs={"article_position": i}
+                on_click=section.move_article_down,
+                kwargs={"article_position": i},
             )
         with c3:
             show_article_expander(article)
@@ -86,6 +94,7 @@ def show_articles(section: Section):
             on_click=layout.unassign_article,
             kwargs={"article_id": article_id, "from_id": section_id},
         )
+
 
 def show_article_expander(article):
     title = article.title
@@ -101,32 +110,26 @@ def show_article_expander(article):
         st.write(f"{preview_text}")
 
 
-
 def get_numbered_list(raw_list, start_with_one=True):
     offset = 1 if start_with_one else 0
-    return [
-        f"({idx+offset})\t{itm}"
-        for idx, itm
-        in enumerate(raw_list)
-    ]
-    
+    return [f"({idx + offset})\t{itm}" for idx, itm in enumerate(raw_list)]
+
+
 def find_move(original, modified):
     """
     For a list with one element moved to a new position, returns the indexes of the old and new positions.
     Returns:
         (old_position, new_position).
     """
-    start = next(
-        (i for i, (a, b) in enumerate(zip(original, modified)) if a != b),
-        None
-    )
+    start = next((i for i, (a, b) in enumerate(zip(original, modified)) if a != b), None)
 
     if start is None:
         ValueError("Lists are identical.")  # no move
 
-    end = len(original) - 1 - next(
-        i for i, (a, b) in enumerate(zip(reversed(original), reversed(modified)))
-        if a != b
+    end = (
+        len(original)
+        - 1
+        - next(i for i, (a, b) in enumerate(zip(reversed(original), reversed(modified))) if a != b)
     )
 
     # Moved right
@@ -138,5 +141,3 @@ def find_move(original, modified):
         return end, start
 
     raise ValueError("Lists are not related by a single move")
-
-    
